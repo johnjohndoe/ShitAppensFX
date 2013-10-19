5296ec2f3f3de2dd5ad743833f471e3f168c526f
Tobias Preuss <tobias.preuss@googlemail.com>
2013-10-19 14:49:29 +0200

    Add basic layout with bound resources.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,33 @@
 package sample;
 
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.shape.Rectangle;
+
 public class Controller {
+
+    @FXML
+    Label labelBox01;
+    @FXML
+    Label labelBox02;
+    @FXML
+    Label labelBox03;
+
+    @FXML
+    Button buttonBox01;
+    @FXML
+    Button buttonBox02;
+    @FXML
+    Button buttonBox03;
+
+    @FXML
+    Rectangle rectangleBox01;
+    @FXML
+    Rectangle rectangleBox02;
+    @FXML
+    Rectangle rectangleBox03;
+
+
 }
