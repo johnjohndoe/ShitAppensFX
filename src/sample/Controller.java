dfa9de705e09eb71e18de5e2c481fc5bfc60b4f5
Tobias Preuss <tobias.preuss@googlemail.com>
2013-10-19 14:51:32 +0200

    Add event handlers.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,9 +1,11 @@
 package sample;
 
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
 public class Controller {
@@ -29,5 +31,37 @@ public class Controller {
     @FXML
     Rectangle rectangleBox03;
 
+    @FXML
+    protected void onButtonBox01Click(ActionEvent actionEvent) {
+        toggleButtonStatus(actionEvent, buttonBox01);
+        toggleLabelStatus(actionEvent, labelBox01);
+        toggleRectangleStatus(actionEvent, rectangleBox01);
+    }
+
+    @FXML
+    protected void onButtonBox02Click(ActionEvent actionEvent) {
+        toggleButtonStatus(actionEvent, buttonBox02);
+        toggleLabelStatus(actionEvent, labelBox02);
+        toggleRectangleStatus(actionEvent, rectangleBox02);
+    }
+
+    @FXML
+    protected void onButtonBox03Click(ActionEvent actionEvent) {
+        toggleButtonStatus(actionEvent, buttonBox03);
+        toggleLabelStatus(actionEvent, labelBox03);
+        toggleRectangleStatus(actionEvent, rectangleBox03);
+    }
+
+    protected void toggleButtonStatus(ActionEvent actionEvent, Button button) {
+        button.setText((button.getText().equals("Reserve")) ? "Release" : "Reserve");
+    }
+
+    protected void toggleLabelStatus(ActionEvent actionEvent, Label label) {
+        label.setText((label.getText().equals("Taken")) ? "Free" : "Taken");
+    }
+
+    protected void toggleRectangleStatus(ActionEvent actionEvent, Rectangle rectangle) {
+        rectangle.setFill((rectangle.getFill() == Color.RED) ? Color.GREEN : Color.RED);
+    }
 
 }
